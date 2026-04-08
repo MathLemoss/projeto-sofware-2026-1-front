28a8f1b061574b2c56bc244884891f612ca96677
MathLemoss <matheussl1@al.insper.edu.br>
2026-04-08 11:02:26 -0300

    configs

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-x65bk1hkee81c774.us.auth0.com"
-      clientId="kFjkpeVhXxhTGY4zKMh4ytjHhOWmdYYc"
+      domain="dev-47408b7hxr8r67xb.us.auth0.com"
+      clientId="XTIP6KEZuPqpoOUbzFc0IvRrU2BhOp9A"
       authorizationParams={{
-        audience: "https://dev-x65bk1hkee81c774.us.auth0.com/api/v2/",
+        audience: "https://dev-47408b7hxr8r67xb.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
